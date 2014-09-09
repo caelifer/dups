7dc4f3527229dd4bed8f1f0a9610ccf63a479e55
Timour Ezeev <timour.ezeev@me.com>
2014-09-09 14:34:02 -0400

    - Splitted balancer implementation into separate files

---
+++ balancer/pool.go
@@ -0,0 +1,58 @@
+package balancer
+
+import (
+	"fmt"
+	"runtime"
+)
+
+var maxWorkerNumber = runtime.NumCPU()
+
+type Pool []*Worker
+
+func NewPool() Pool {
+	return make([]*Worker, 0, maxWorkerNumber)
+}
+
+func (p Pool) getStats() []int {
+	stats := make([]int, len(p))
+	for i := 0; i < len(p); i++ {
+		stats[i] = p[i].QueueSize()
+	}
+	return stats
+}
+
+func (p Pool) String() string {
+	stats := p.getStats()
+	return fmt.Sprintf("pool: %+v, load: {avg: %.1f, mean: %.1f, stddev: %.1f}",
+		stats, avg(stats), mean(stats), stddev(stats))
+}
+
+func (p Pool) Cap() int {
+	return cap(p)
+}
+
+// Implement heap.Interface
+func (p Pool) Len() int           { return len(p) }
+func (p Pool) Less(i, j int) bool { return p[i].QueueSize() < p[j].QueueSize() }
+func (p Pool) Swap(i, j int) {
+	p[i], p[j] = p[j], p[i]
+	p[i].index = i
+	p[j].index = j
+}
+
+func (p *Pool) Push(x interface{}) {
+	// log.Println("Adding new element to the heap")
+	w := x.(*Worker)
+	w.index = len(*p)
+	*p = append(*p, w)
+	// log.Println(p)
+}
+
+func (p *Pool) Pop() interface{} {
+	old := *p
+	n := len(old) - 1
+	x := old[n]
+	x.index = -1 // for safety
+	*p = old[0:n]
+	return x
+}
---
+++ balancer/worker.go
@@ -0,0 +1,41 @@
+package balancer
+
+import "errors"
+
+type Request func()
+
+type Worker struct {
+	reqs    chan Request
+	pending int
+	index   int // heap index
+}
+
+const maxWorkQueueDepth = 10
+
+func NewWorker() *Worker {
+	return &Worker{
+		reqs: make(chan Request, maxWorkQueueDepth),
+	}
+}
+
+func (w *Worker) Enqueue(r Request) error {
+	// Add Request to the queue
+	select {
+	case w.reqs <- r:
+		return nil
+	default:
+		return errors.New("job queue capacity limit")
+	}
+}
+
+func (w Worker) QueueSize() int {
+	return w.pending
+}
+
+func (w *Worker) Run(done chan *Worker) {
+	for {
+		req := <-w.reqs
+		req() // run job
+		done <- w
+	}
+}
