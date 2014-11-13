95f3d889f37e4fb0d3c916fb2a2c7b97a9daa724
Timour Ezeev <timour.ezeev@me.com>
2014-11-13 18:58:48 -0500

    - Split type definitions into its onw file

--- mapreduce/mapreduce.go
+++ mapreduce/mapreduce.go
@@ -1,78 +1,13 @@
 package mapreduce
 
-import "strconv"
-
-// Value interface for objects that Map expects it its input stream
-type Value interface {
-	Value() interface{}
-}
-
-// Key interace is there to make sure that the value can be used in go built-in
-// map as a key.
-type Key interface {
-	Key() KeyType
-}
-
-// KeyValue interface describes objects produced by Map and used by Reduce as
-// its input stream
-type KeyValue interface {
-	Key
-	Value
-}
-
-// Helper concrete types for Key and KeyValue interfaces
-
-type KeyType string
-
-func (kt KeyType) Key() KeyType {
-	return kt
-}
-
-func (kt KeyType) String() string {
-	return string(kt)
-}
-
-// Helper functions
-
-func KeyTypeFromString(s string) KeyType {
-	return KeyType(s)
-}
-
-func KeyTypeFromInt64(i int64) KeyType {
-	return KeyType(strconv.FormatInt(i, 10))
-}
-
-func KeyTypeFromInt(i int) KeyType {
-	return KeyType(strconv.FormatInt(int64(i), 10))
-}
-
-type KVType struct {
-	key Key
-	val Value
-}
-
-func NewKVType(k Key, v Value) *KVType {
-	return &KVType{
-		key: k,
-		val: v,
-	}
-}
-
-func (kvt KVType) Key() KeyType {
-	return kvt.key.Key()
-}
-
-func (kvt KVType) Value() interface{} {
-	return kvt.val.Value()
-}
-
 // Map-Reduce implementation
 
 // MapFn provided by the client code. It is responsible to perform actual work
 // and send it to the out channel as KeyValue tuple.
 type MapFn func(out chan<- KeyValue)
 
-// Map is a primary interface to Map functionality
+// Map run provided Key/Value generator in a separate go-routine. It returns channel of
+// KeyValue values.
 func Map(mapFn MapFn) <-chan KeyValue {
 	out := make(chan KeyValue)
 	go func() {
@@ -84,9 +19,11 @@ func Map(mapFn MapFn) <-chan KeyValue {
 
 // ReduceFn a function provided by the client code. It expects to aggregate
 // over the values povided by the in channel of KeyValue objects and send its
-// result to out Value channel
+// result to out Value channel.
 type ReduceFn func(out chan<- Value, in <-chan KeyValue)
 
+// Reduce takes input KeyValue channel and reduce function. It returns Value channel
+// used by reducer for output.
 func Reduce(in <-chan KeyValue, reduceFn ReduceFn) <-chan Value {
 	out := make(chan Value)
 	go func() {
---
+++ mapreduce/types.go
@@ -0,0 +1,67 @@
+package mapreduce
+
+import "strconv"
+
+// Value interface for objects that Map expects it its input stream
+type Value interface {
+	Value() interface{}
+}
+
+// Key interace is there to make sure that the value can be used in go built-in
+// map as a key.
+type Key interface {
+	Key() KeyType
+}
+
+// KeyValue interface describes objects produced by Map and used by Reduce as
+// its input stream
+type KeyValue interface {
+	Key
+	Value
+}
+
+// Helper concrete types for Key and KeyValue interfaces
+
+type KeyType string
+
+func (kt KeyType) Key() KeyType {
+	return kt
+}
+
+func (kt KeyType) String() string {
+	return string(kt)
+}
+
+// Helper functions
+
+func KeyTypeFromString(s string) KeyType {
+	return KeyType(s)
+}
+
+func KeyTypeFromInt64(i int64) KeyType {
+	return KeyType(strconv.FormatInt(i, 10))
+}
+
+func KeyTypeFromInt(i int) KeyType {
+	return KeyType(strconv.FormatInt(int64(i), 10))
+}
+
+type KVType struct {
+	key Key
+	val Value
+}
+
+func NewKVType(k Key, v Value) *KVType {
+	return &KVType{
+		key: k,
+		val: v,
+	}
+}
+
+func (kvt KVType) Key() KeyType {
+	return kvt.key.Key()
+}
+
+func (kvt KVType) Value() interface{} {
+	return kvt.val.Value()
+}
