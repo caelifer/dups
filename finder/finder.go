15eb1be37ddb097bc226df76571f0635710929d4
Timour Ezeev <timour.ezeev@me.com>
2014-11-15 15:48:14 -0500

    Split Dups type definition into its own file

--- finder/finder.go
+++ finder/finder.go
@@ -14,22 +14,6 @@ import (
 	"github.com/caelifer/dups/node"
 )
 
-// Dup type describes found duplicate file
-type Dup struct {
-	*node.Node     // Embed Node type Go type "inheritance"
-	Count      int // Number of identical copies for the hash
-}
-
-// Value implements mapreduce.Value interface
-func (d Dup) Value() interface{} {
-	return d
-}
-
-// Pretty printer for the report
-func (d Dup) String() string {
-	return fmt.Sprintf("%s:%d:%d:%q", d.Hash, d.Count, d.Size, d.Path)
-}
-
 // Finder
 type Finder struct {
 	// Work Queue
